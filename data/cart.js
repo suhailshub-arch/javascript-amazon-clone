186d6d4c4f58d508fe7b32453386a2dc451b062d
Suhail Sallahudin <suhail.sallahudin@gmail.com>
2025-05-16 18:29:22 +0100

    Add interactivity to checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,49 @@
 import {products} from './products.js';
 
+// export const cart = {
+//     items: [],
+//     totalQuantity: 0,
+//     totalPriceCents: 0
+// };
+
 export const cart = {
-    items: [],
+    items: [
+        {
+            product :{
+                id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+                name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+                rating: {
+                stars: 4.5,
+                count: 87
+                },
+                priceCents: 1090,
+                keywords: [
+                "socks",
+                "sports",
+                "apparel"
+                ]
+            },
+            quantity: 2
+        },
+        {
+            product :{
+                id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                image: "images/products/intermediate-composite-basketball.jpg",
+                name: "Intermediate Size Basketball",
+                rating: {
+                stars: 4,
+                count: 127
+                },
+                priceCents: 2095,
+                keywords: [
+                "sports",
+                "basketballs"
+                ]
+            },
+            quantity: 1
+            },
+    ],
     totalQuantity: 0,
     totalPriceCents: 0
 };
